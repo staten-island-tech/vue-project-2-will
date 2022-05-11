b6a2699bbc2da31e9ff01c98ca1f2a396d290459
William020304 <WIlliamc248@nycstudents.net>
2022-05-11 09:40:35 -0400

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 
-// global styles
-import './assets/main.css'
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: Signup
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
-]
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
